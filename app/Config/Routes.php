41b0bf8308c701e153513f70df332e2e50450d34
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-02 18:51:07 -0300

    feat: adiciona rota de clientes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Controllers\ClienteController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group('api', function (RouteCollection $routes) {
+
+    $routes->group('clientes', function (RouteCollection $routes) {
+        $routes->get('', [ClienteController::class, 'index']);
+        $routes->get('(:num)', [ClienteController::class, 'show']);
+        $routes->post('', [ClienteController::class, 'store']);
+    });
+});
