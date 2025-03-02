dae6f87d9b56fe418a50d3572e3e9c9877477695
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-02 18:41:29 -0300

    feat: adiciona classe base para validação de entradas no controller

---
+++ app/Validators/AbstractValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Validators;
+
+use App\Helpers\ArrayKeys;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\Validation\ValidationInterface;
+
+abstract class AbstractValidator
+{
+    protected ValidationInterface $validator;
+    protected IncomingRequest $request;
+    protected array $errors = [];
+
+    public function __construct(
+        IncomingRequest $request,
+        ValidationInterface $validator
+    ) {
+        $this->request = $request;
+        $this->validator = $validator;
+    }
+
+    abstract protected function rules(): array;
+
+    public function validate(): bool
+    {
+        $this->validator->setRules($this->rules());
+
+        $isValid = $this->validator->withRequest($this->request)->run();
+
+        $this->errors = $this->validator->getErrors();
+
+        return $isValid;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function getValidatedData(): array
+    {
+        return ArrayKeys::toSnakeCase($this->validator->getValidated());
+    }
+}
