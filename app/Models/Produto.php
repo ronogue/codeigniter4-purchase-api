9e2b973563706aeac04ea9ba511b0db007f3612f
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 10:48:08 -0300

    feat: adiciona model para produtos

---
+++ app/Models/Produto.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+class Produto extends BaseModel
+{
+    protected $table            = 'produtos';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+
+    protected $allowedFields    = [
+        'nome',
+        'preco',
+        'descricao',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
