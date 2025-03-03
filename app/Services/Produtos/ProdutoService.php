dd3729794d6337119b6452bfdaea8e680d73de49
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 11:07:24 -0300

    feat: adiciona service de crud para produtos

---
+++ app/Services/Produtos/ProdutoService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services\Produtos;
+
+use App\Entities\PagedData;
+use App\Models\ProdutoModel;
+use App\Entities\Produtos\Produto;
+
+class ProdutoService implements ProdutoServiceInterface
+{
+    private ProdutoModel $produtoModel;
+
+    public function __construct()
+    {
+        $this->produtoModel = model(ProdutoModel::class);
+    }
+
+    public function getAllPaginated(int $page, int $perPage = 10): PagedData
+    {
+        return $this->produtoModel->findAllPaginated($page, $perPage);
+    }
+
+    public function getById(int $id): ?Produto
+    {
+        return $this->produtoModel->find($id);
+    }
+
+    public function create(array $data): Produto
+    {
+        $dataToCreate = $this->produtoModel->filterByAllowedFields($data);
+        $this->produtoModel->insert($dataToCreate);
+
+        return $this->getById($this->produtoModel->getInsertID());
+    }
+
+    public function update(int $id, array $data): Produto
+    {
+        $dataToUpdate = $this->produtoModel->filterByAllowedFields($data);
+
+        if (count($dataToUpdate) === 0) {
+            return $this->getById($id);
+        }
+
+        $this->produtoModel->update($id, $dataToUpdate);
+
+        return $this->getById($id);
+    }
+
+    public function delete(int $id): bool
+    {
+        return $this->produtoModel->delete($id);
+    }
+}
---
+++ app/Services/Produtos/ProdutoServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\Produtos;
+
+use App\Services\CrudServiceInterface;
+
+/**
+ * @template T of \App\Entities\Produtos\Produto
+ * @package App\Services\Produtos
+ */
+interface ProdutoServiceInterface extends CrudServiceInterface {}
