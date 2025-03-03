ba51023a77830b3f0054e67551bca0fc2e1e3c2a
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 11:06:47 -0300

    refactor: altera o namespace

---
+++ app/Services/CrudServiceInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\PagedData;
+use App\Entities\BaseEntity;
+
+/**
+ * @template T of BaseEntity
+ * @package App\Services
+ */
+interface CrudServiceInterface
+{
+    /**
+     * @param int $page número da página
+     * @param int $perPage quantidade de itens por página
+     * 
+     * @return PagedData 
+     */
+    public function getAllPaginated(int $page, int $perPage = 10): PagedData;
+
+    /**
+     * @param int $id id do registro
+     * @return T|null 
+     */
+    public function getById(int $id): ?BaseEntity;
+
+    /**
+     * @param array $data dados do registro
+     * @return T 
+     */
+    public function create(array $data): BaseEntity;
+
+    /**
+     * @param int $id id do registro
+     * @param array $data dados do registro
+     * @return T 
+     */
+    public function update(int $id, array $data): BaseEntity;
+
+    /**
+     * @param int $id id do registro
+     * @return bool true se foi excluído com sucesso
+     */
+    public function delete(int $id): bool;
+}
