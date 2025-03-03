f000825726859de393491e15aab3724455e7cbaa
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 10:38:49 -0300

    refactor: adiciona extensão à CrudServiceInterface

--- app/Services/Clientes/ClienteServiceInterface.php
+++ app/Services/Clientes/ClienteServiceInterface.php
@@ -2,18 +2,4 @@
 
 namespace App\Services\Clientes;
 
-use App\Entities\PagedData;
-use App\Entities\Clientes\AbstractCliente;
-
-interface ClienteServiceInterface
-{
-    public function getAll(): PagedData;
-
-    public function getById(int $id): ?AbstractCliente;
-
-    public function create(array $data): AbstractCliente;
-
-    public function update(int $id, array $data): AbstractCliente;
-
-    public function delete(int $id): bool;
-}
+interface ClienteServiceInterface extends CrudServiceInterface {}
