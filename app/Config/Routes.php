855e9dbfcb0b8c7933f31e245faa17c77f5d54e1
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 10:30:37 -0300

    feat: adiciona rotas para atualização e remoção de clientes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,8 +9,12 @@ use CodeIgniter\Router\RouteCollection;
 $routes->group('api', function (RouteCollection $routes) {
 
     $routes->group('clientes', function (RouteCollection $routes) {
+
         $routes->get('', [ClienteController::class, 'index']);
         $routes->get('(:num)', [ClienteController::class, 'show']);
         $routes->post('', [ClienteController::class, 'store']);
+        $routes->put('', [ClienteController::class, 'update']);
+        $routes->patch('', [ClienteController::class, 'update']);
+        $routes->delete('(:num)', [ClienteController::class, 'delete']);
     });
 });
