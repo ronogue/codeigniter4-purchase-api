5a851b8efa8d09fa588294f45834d11d3d9f2696
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 11:08:01 -0300

    feat: adiciona docstring

--- app/Entities/Produtos/Produto.php
+++ app/Entities/Produtos/Produto.php
@@ -4,4 +4,12 @@ namespace App\Entities\Produtos;
 
 use App\Entities\BaseEntity;
 
+/**
+ * @property int $id
+ * @property string $nome
+ * @property float $preco
+ * @property string $descricao
+ * 
+ * @package App\Entities\Produtos
+ */
 class Produto extends BaseEntity {}
