cbe1d270142b4d023c9534b90418bd6c148144d8
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 12:06:50 -0300

    feat: adiciona modelos para pedido e seus itens

---
+++ app/Models/Pedidos/PedidoModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Pedidos;
+
+use CodeIgniter\Model;
+use App\Models\BaseModel;
+
+class PedidoModel extends BaseModel
+{
+    protected $table            = 'pedidos';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
