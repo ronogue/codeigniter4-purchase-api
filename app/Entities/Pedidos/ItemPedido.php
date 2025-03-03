f2292fd6c6f7b06538931a4c3fcca7b1b1473c83
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2025-03-03 14:19:07 -0300

    feat: adiciona mapeamento de dados

--- app/Entities/Pedidos/ItemPedido.php
+++ app/Entities/Pedidos/ItemPedido.php
@@ -4,4 +4,19 @@ namespace App\Entities\Pedidos;
 
 use App\Entities\BaseEntity;
 
-class ItemPedido extends BaseEntity {}
+class ItemPedido extends BaseEntity
+{
+    public function __construct(?array $data = null)
+    {
+        parent::__construct($data);
+
+        $this->datamap = array_merge(
+            $this->datamap,
+            [
+                'produtoId' => 'produto_id',
+                'pedidoId' => 'pedido_id',
+                'precoUnitario' => 'preco_unitario',
+            ]
+        );
+    }
+}
--- app/Entities/Pedidos/Pedido.php
+++ app/Entities/Pedidos/Pedido.php
@@ -4,4 +4,18 @@ namespace App\Entities\Pedidos;
 
 use App\Entities\BaseEntity;
 
-class Pedido extends BaseEntity {}
+class Pedido extends BaseEntity
+{
+    public function __construct(?array $data = null)
+    {
+        parent::__construct($data);
+
+        $this->datamap = array_merge(
+            $this->datamap,
+            [
+                'clienteId' => 'cliente_id',
+                'dataPedido' => 'data_pedido',
+            ]
+        );
+    }
+}
